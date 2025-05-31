658c987ba75eb00571c2263fa5ec4ae562034261
Koded0214h <coder0214h@gmail.com>
2025-05-31 16:37:12 +0100

    Update ALLOWED_HOSTS and ensure services.py is properly configured

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'newsly-ai.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
